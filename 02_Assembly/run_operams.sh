7d1117d7cfaa705940ab29a36bdaf4946f181533
seunji-kfri <songej486@gmail.com>
2025-08-30 18:44:46 +0900

    Update run_operams.sh

--- 02_Assembly/run_operams.sh
+++ 02_Assembly/run_operams.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Usage: run_operams.sh <long_read_file> <short_srr_1> <short_srr_2> <output_directory>
+# <long_read_file> : The long-read file (e.g., SAMPLE01.nohuman.fastq)
+# <short_srr_1> : The prefix for short-read file 1 (e.g., SAMPLE01_1.nohuman.fq.gz)
+# <short_srr_2> : The prefix for short-read file 2 (e.g., SAMPLE01_2.nohuman.fq.gz)
+# <output_directory> : The directory to store the output (e.g., 02_Assembly/out/SAMPLE01_hybrid)
 
 # Number of threads (adjust as necessary)
 CPU=40
