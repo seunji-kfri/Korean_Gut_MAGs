bc6833ee5cd93e76d7e828abfc0901d078f996df
seunji-kfri <songej486@gmail.com>
2025-08-30 18:07:55 +0900

    Update run_metawrap.sh

--- 03_Binning/run_metawrap.sh
+++ 03_Binning/run_metawrap.sh
@@ -1,36 +1,41 @@
 #!/bin/bash
 # Usage: run_metawrap.sh <sample_id> <short_srr>
 
-. ~/enable_conda.rc
-conda activate metawrap
+# Activate conda environment (ensure the environment created with 'environment.yml' is available)
+conda activate KoreanGutMAGs  # Ensure this is the environment created with 'environment.yml'
 
-J=$1
-short_srr=$2
+# Define input/output paths based on the directory structure in the project
+ASSEMBLY_DIR=./02_Assembly/out   # Path to assembly results from 02_Assembly
+FINALDIR=./operams_results  # Final output directory for binning results
+FASTQ_DIR=./preprocessed_data  # Preprocessed data directory from 01_QC
 
+# Define input/output files
+SAMPLE=$1
+SHORT_SRR=$2
 
-WORKDIR=/tmp/${J}.bins
-FINALDIR=/home/caefs/microbiome/projects/metagenome_analysis/China/operams
-FASTQ_DIR=/home/caefs/microbiome/projects/metagenome_analysis/China/preprocess
-BASE=/home/caefs/microbiome/projects/metagenome_analysis/China/operams/${J}
-FASTQ1_ORIG=${FASTQ_DIR}/${short_srr}_1.nohuman.fq.gz
-FASTQ2_ORIG=${FASTQ_DIR}/${short_srr}_2.nohuman.fq.gz
-ASSEMBLY_ORIG=${BASE}/contigs.polished.fasta
+# Correct path to the assembly output
+ASSEMBLY=${ASSEMBLY_DIR}/${SAMPLE}_hybrid/contigs_polished.fasta  # Assembly from 02_Assembly (hybrid output)
 
+# Input files (should be user-provided)
+FASTQ1_ORIG=${FASTQ_DIR}/${SHORT_SRR}_1.nohuman.fq.gz
+FASTQ2_ORIG=${FASTQ_DIR}/${SHORT_SRR}_2.nohuman.fq.gz
 
-LOGDIR=/home/caefs/microbiome/projects/metagenome_analysis/China
+LOGDIR=./03_Binning/logs  # Directory to store logs
 
+# Create necessary directories
+mkdir -p $FINALDIR
+mkdir -p $LOGDIR
 
-mkdir -p $WORKDIR
-cd $WORKDIR
+# Copy input files to the working directory
+cp $FASTQ1_ORIG ./reads_1.fq.gz
+cp $FASTQ2_ORIG ./reads_2.fq.gz
+cp $ASSEMBLY ./assembly.fasta
 
-
-cp $FASTQ1_ORIG ${WORKDIR}/reads_1.fq.gz
-cp $FASTQ2_ORIG ${WORKDIR}/reads_2.fq.gz
-cp $ASSEMBLY_ORIG ${WORKDIR}/assembly.fasta
-
+# Unzip the fastq files
 gunzip reads_1.fq.gz
 gunzip reads_2.fq.gz
 
+# Rename fastq files to .fastq
 mv reads_1.fq reads_1.fastq
 mv reads_2.fq reads_2.fastq
 
@@ -41,7 +46,7 @@ metawrap binning \
   -a assembly.fasta \
   --metabat2 --maxbin2 --concoct \
   reads_1.fastq reads_2.fastq \
-  &> ${LOGDIR}/${J}.binning.log
+  &> $LOGDIR/${SAMPLE}.binning.log
 
 # Step 2: Refinement
 metawrap bin_refinement \
@@ -51,12 +56,16 @@ metawrap bin_refinement \
   -A binning/metabat2_bins \
   -B binning/maxbin2_bins \
   -C binning/concoct_bins \
-  &> ${LOGDIR}/${J}.refine.log
+  &> $LOGDIR/${SAMPLE}.refine.log
 
+# Clean up intermediate files
 rm -f reads_1.fastq reads_2.fastq assembly.fasta
 
-mv ${WORKDIR}/* ${FINALDIR}/${J}.bins
-rm -rf ${WORKDIR}
+# Move results to the final output directory
+mkdir -p $FINALDIR/${SAMPLE}
+mv $WORKDIR/binning/refined/* $FINALDIR/${SAMPLE}.bins
 
+# Clean up working directory
+rm -rf binning
 
-echo "[INFO] Binning and refinement for ${J} completed and moved to ${FINALDIR}/${J}.bins"
+echo "[INFO] Binning and refinement for ${SAMPLE} completed and moved to ${FINALDIR}/${SAMPLE}.bins"
