e33c7e0e896a45ebd0e7d8bbba9077775dbb40f5
seunji-kfri <songej486@gmail.com>
2025-08-30 18:14:49 +0900

    Update run_metawrap.sh

--- 03_Binning/run_metawrap.sh
+++ 03_Binning/run_metawrap.sh
@@ -6,8 +6,8 @@ conda activate KoreanGutMAGs  # Ensure this is the environment created with 'env
 
 # Define input/output paths based on the directory structure in the project
 ASSEMBLY_DIR=./02_Assembly/out   # Path to assembly results from 02_Assembly
-FINALDIR=./operams_results  # Final output directory for binning results
-FASTQ_DIR=./preprocessed_data  # Preprocessed data directory from 01_QC
+BINNING_DIR=./03_Binning/out  # Final output directory for binning results (changed to BINNING_DIR)
+FASTQ_DIR=./01_QC/out  # Preprocessed data directory from 01_QC
 
 # Define input/output files
 SAMPLE=$1
@@ -20,52 +20,29 @@ ASSEMBLY=${ASSEMBLY_DIR}/${SAMPLE}_hybrid/contigs_polished.fasta  # Assembly fro
 FASTQ1_ORIG=${FASTQ_DIR}/${SHORT_SRR}_1.nohuman.fq.gz
 FASTQ2_ORIG=${FASTQ_DIR}/${SHORT_SRR}_2.nohuman.fq.gz
 
-LOGDIR=./03_Binning/logs  # Directory to store logs
-
-# Create necessary directories
-mkdir -p $FINALDIR
-mkdir -p $LOGDIR
-
-# Copy input files to the working directory
-cp $FASTQ1_ORIG ./reads_1.fq.gz
-cp $FASTQ2_ORIG ./reads_2.fq.gz
-cp $ASSEMBLY ./assembly.fasta
+# Create necessary directories for final output
+mkdir -p $BINNING_DIR/${SAMPLE}
 
 # Unzip the fastq files
-gunzip reads_1.fq.gz
-gunzip reads_2.fq.gz
-
-# Rename fastq files to .fastq
-mv reads_1.fq reads_1.fastq
-mv reads_2.fq reads_2.fastq
+gunzip -c $FASTQ1_ORIG > ./reads_1.fastq
+gunzip -c $FASTQ2_ORIG > ./reads_2.fastq
 
 # Step 1: Binning
 metawrap binning \
-  -o binning \
-  -t 40 -m 200 \
-  -a assembly.fasta \
+  -o $BINNING_DIR/${SAMPLE}.bins \
+  -a $ASSEMBLY \
   --metabat2 --maxbin2 --concoct \
-  reads_1.fastq reads_2.fastq \
-  &> $LOGDIR/${SAMPLE}.binning.log
+  ./reads_1.fastq ./reads_2.fastq
 
 # Step 2: Refinement
 metawrap bin_refinement \
-  -o binning/refined \
-  -t 40 -m 200 \
+  -o $BINNING_DIR/${SAMPLE}.bins/refined \
   -c 50 -x 10 \
-  -A binning/metabat2_bins \
-  -B binning/maxbin2_bins \
-  -C binning/concoct_bins \
-  &> $LOGDIR/${SAMPLE}.refine.log
+  -A $BINNING_DIR/${SAMPLE}.bins/metabat2_bins \
+  -B $BINNING_DIR/${SAMPLE}.bins/maxbin2_bins \
+  -C $BINNING_DIR/${SAMPLE}.bins/concoct_bins
 
 # Clean up intermediate files
-rm -f reads_1.fastq reads_2.fastq assembly.fasta
+rm -f ./reads_1.fastq ./reads_2.fastq $ASSEMBLY
 
-# Move results to the final output directory
-mkdir -p $FINALDIR/${SAMPLE}
-mv $WORKDIR/binning/refined/* $FINALDIR/${SAMPLE}.bins
-
-# Clean up working directory
-rm -rf binning
-
-echo "[INFO] Binning and refinement for ${SAMPLE} completed and moved to ${FINALDIR}/${SAMPLE}.bins"
+echo "[INFO] Binning and refinement for ${SAMPLE} completed and saved to ${BINNING_DIR}/${SAMPLE}.bins"
