621fa63d98752863735953338acd0cba9e29fd4e
seunji-kfri <songej486@gmail.com>
2025-08-30 18:52:07 +0900

    Update run_flye.sh

--- 02_Assembly/run_flye.sh
+++ 02_Assembly/run_flye.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# Usage: run_flye.sh <sample_id>
+# Usage: run_flye.sh <sample_id> <output_folder>
 # <sample_id> : The ID of the sample (e.g., SAMPLE01)
+# <output_folder> : The directory where Flye results will be saved (e.g., 02_Assembly/out/SAMPLE01)
 
 # Number of threads (adjust as necessary)
 CPU=20
@@ -9,7 +10,7 @@ CPU=20
 conda activate KoreanGutMAGs  # Ensure this is the environment created with 'environment.yml'
 
 # Define output directory
-OUTPUT_DIR=$1   # Use the sample ID as the output directory
+OUTPUT_DIR=$2   # Use the second argument as the output directory
 
 # Run Flye assembly (ONT long-read)
 flye --meta --nano-raw /path/to/your/input/${1}.nohuman.fastq --out-dir $OUTPUT_DIR --threads $CPU
