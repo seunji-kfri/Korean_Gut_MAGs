21edcfbd31948da64b814dc03bc1a417d4e6e8ef
seunji-kfri <songej486@gmail.com>
2025-08-30 18:25:09 +0900

    Update run_flye.sh

--- 02_Assembly/run_flye.sh
+++ 02_Assembly/run_flye.sh
@@ -3,8 +3,8 @@
 # Number of threads (adjust as necessary)
 CPU=20
 
-# Activate conda environment (make sure the environment created via 'environment.yml' is available)
-conda activate KoreanGutMAGs   # Example: Change this to the actual environment name
+# Activate conda environment (ensure the environment created via 'environment.yml' is available)
+conda activate KoreanGutMAGs  # Ensure this is the environment created with 'environment.yml'
 
 # Define output directory
 OUTPUT_DIR=$1   # Use the sample ID as the output directory
